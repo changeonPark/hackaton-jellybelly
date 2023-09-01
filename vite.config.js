789ce9e3aedf47c68c922d5f123688dbab44e2d8
changeonPark <qkrcksrjs@gmail.com>
2023-09-01 14:43:28 +0900

    test: sw

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        sw: "./sw.js",
-      },
-    },
-  },
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
 })
